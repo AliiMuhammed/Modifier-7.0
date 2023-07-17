d78fb9fcf44313e45d0c41999b50b0de998dc5d2
Abdelrahman435 <ah0383723@gmail.com>
2023-07-17 04:28:52 +0300

    database

--- back-end/DB/parking.sql
+++ back-end/DB/parking.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 13, 2023 at 10:49 PM
+-- Generation Time: Jul 17, 2023 at 03:27 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -20,6 +20,71 @@ SET time_zone = "+00:00";
 --
 -- Database: `parking`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `aboutus`
+--
+
+CREATE TABLE `aboutus` (
+  `id` int(11) NOT NULL,
+  `member_Name` varchar(255) NOT NULL,
+  `member_Role` varchar(255) NOT NULL,
+  `member_img` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `aboutus`
+--
+
+INSERT INTO `aboutus` (`id`, `member_Name`, `member_Role`, `member_img`, `description`) VALUES
+(1, 'Abdo Hassan', 'Admin', '1689551058133.jpg', 'sffdsxscscvxcvcfbcxbfxbcbmn'),
+(2, 'Abdo', 'admin', '1689551069125.jpg', 'gcfhvbmnkbhjnmzsxdcfvhgbjntybn');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contactus`
+--
+
+CREATE TABLE `contactus` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `message` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `contactus`
+--
+
+INSERT INTO `contactus` (`id`, `name`, `email`, `message`) VALUES
+(1, 'abdelrahman', 'ah@gamil.com', 'fdm,xcvvxcvcxxvx'),
+(2, 'abdelrahman', 'ah@gamil.com', 'fdm,xcvvxcvcxxvx');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedback`
+--
+
+CREATE TABLE `feedback` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `rating` int(1) NOT NULL,
+  `comment` varchar(255) NOT NULL,
+  `services` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `feedback`
+--
+
+INSERT INTO `feedback` (`id`, `user_id`, `rating`, `comment`, `services`) VALUES
+(1, 2, 4, 'jhbmn.,hbj', 'park'),
+(2, 3, 6, 'jhbmn.,hbj', 'park');
 
 -- --------------------------------------------------------
 
@@ -45,11 +110,32 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `phone`, `image`, `last-dest`, `last-date`, `last-cost`, `time-notficaition`) VALUES
-(1, 'Abdo', 'ah@gmail.com', '$2b$10$OkTJdJ1Y3XvWRM5NLxouL.YrB5mKwqNV40ZByb4LCFmGRlsPkIt5m', 1210201342, '', '', '0000-00-00', 0, '');
+(2, 'Abdo', 'ah@gmail.com', '$2b$10$YKyUaLE3cbNpdMxUqUg10uQBfCVwcaj7xLiPiBWH98hofauFnF5se', 1210201342, '1689287590602.jpg', '', '0000-00-00', 0, ''),
+(3, 'Abdo', 'ah1@gmail.com', '$2b$10$iKMQpMsXqSk418N/WCiNTOedACsSyYZEWfZ5JoO9PhWXE46A/VrTe', 1210201342, '', '', '0000-00-00', 0, ''),
+(4, 'Abdo', 'ah2@gmail.com', '$2b$10$tTMrK4/BQCjW18C5ljXAx.5wBsM0FuwWPRBNBcsvazo0EmhlnV9O2', 1210201342, '', '', '0000-00-00', 0, '');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `aboutus`
+--
+ALTER TABLE `aboutus`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `contactus`
+--
+ALTER TABLE `contactus`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `userConst` (`user_id`);
 
 --
 -- Indexes for table `users`
@@ -62,10 +148,38 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `aboutus`
+--
+ALTER TABLE `aboutus`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `contactus`
+--
+ALTER TABLE `contactus`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `feedback`
+--
+ALTER TABLE `feedback`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD CONSTRAINT `userConst` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
