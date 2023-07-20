c852cf7084b6a7defc419fe9c2d7269df4bc50e4
AliiMuhammed <mohamedtaha8000@gmail.com>
2023-07-20 15:18:28 +0300

    last update
    -add storage
    -add navigation from login and sign up
    -redesign nav bar

--- intellicity/src/Shared/NavBar.jsx
+++ intellicity/src/Shared/NavBar.jsx
@@ -1,15 +1,18 @@
-import React, { useEffect } from "react";
-import { Link, useLocation } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useLocation, NavLink } from "react-router-dom";
 import { Dropdown } from "react-bootstrap";
 import logo from "../Assest/Images/h-logo.png";
 import "../Style/nav.css";
 import profile from "../Assest/Images/profile.jpg";
-
-// Rest of the code...
+import { AiFillCloseSquare } from "react-icons/ai";
+import { FaBars } from "react-icons/fa";
+import { getAuthUser, removeAuthUser } from "../Helper/Storage";
+import { useNavigate } from "react-router-dom";
 
 const NavBar = () => {
+  const auth = getAuthUser();
   const location = useLocation();
-
+  const navigate = useNavigate();
   useEffect(() => {
     const targetId = location.hash.substr(1);
     const targetElement = document.getElementById(targetId);
@@ -25,58 +28,133 @@ const NavBar = () => {
     }
   }, [location]);
 
+  const LogOut = () => {
+    removeAuthUser();
+    navigate("/");
+  };
+
+  const [isNavShowing, setIsNavShowing] = useState(false);
+  const links = [
+    {
+      id: 1,
+      name: "Home",
+      path: "/#home",
+    },
+    {
+      id: 2,
+      name: "Our Services",
+      path: "/#our-services",
+    },
+    {
+      id: 3,
+      name: "About Us",
+      path: "/#about",
+    },
+    {
+      id: 4,
+      name: "Contact Us",
+      path: "/#contact-us",
+    },
+  ];
+
   return (
     <nav>
-      <div className="container">
-        <div className="logo">
-          <Link to={"/#home"}>
-            <img src={logo} alt="logo" />
-          </Link>
-        </div>
-        <ul className="nav-links">
-          <li>
-            <Link to="/#home">Home</Link>
-          </li>
-          <li>
-            <Link to="/#about">About us</Link>
-          </li>
-          <li>
-            <Link to="/#our-services">Our Services</Link>
-          </li>
-          <li>
-            <Link to="/#contact-us">Contact Us</Link>
-          </li>
-        </ul>
-        <div className="nav-btns">
-          <Link className="main-btn login-main-btn" to="/login">
-            Login
-          </Link>
-          <Link className="main-btn sign-main-btn" to="/sign-up">
-            Sign Up
-          </Link>
-        </div>
+      <div className="container nav-container">
+        <Link
+          to="/#home"
+          className="logo"
+          onClick={() => setIsNavShowing(false)}
+        >
+          <img src={logo} alt="logo" />
+        </Link>
 
-        {/* <div className="profile">
-          <Dropdown>
-            <Dropdown.Toggle id="dropdown-basic" className="profile-btn">
-              <img src={profile} alt="" />
-            </Dropdown.Toggle>
-            <Dropdown.Menu
-            className="drop-down-profile"
-              style={{
-                transform: "translate(-50%, 0)",
-                top: "100%",
-                left: "50%",
-              }}
-            >
-              <Dropdown.Item href="#/action-1">Your Profile</Dropdown.Item>
-              <Dropdown.Divider />
-              <Dropdown.Item as={Link} to="/logout" className="btn login-btn">
-                Log out
-              </Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-        </div> */}
+        <ul className={`nav-links ${isNavShowing ? "show-nav" : "hide-nav"}`}>
+          {links.map((link) => {
+            return (
+              <li key={link.id}>
+                <NavLink
+                  to={link.path}
+                  onClick={() => setIsNavShowing((prev) => !prev)}
+                >
+                  {link.name}
+                </NavLink>
+              </li>
+            );
+          })}
+
+          {/* Authenticated Routes */}
+          {auth && auth.type !== "admin" && (
+            <>
+              <li className="profile-circle">
+                <div className="profile">
+                  <NavLink to={"/profile" + "/" + auth.type}>
+                    <img src={profile} alt="" />
+                  </NavLink>
+                </div>
+              </li>
+              <li className={"profile-link"}>
+                <NavLink
+                  to={"/profile" + "/" + auth.type}
+                  onClick={() => setIsNavShowing((prev) => !prev)}
+                >
+                  My Profile
+                </NavLink>
+              </li>
+            </>
+          )}
+          {/* Authenticated Routes */}
+          {auth && auth.type === "admin" && (
+            <li>
+              <NavLink to={"/admin"}>DashBoard</NavLink>
+            </li>
+          )}
+          {auth && (
+            <li>
+              <NavLink
+                className={"main-btn login-btn nav-btn  "}
+                onClick={() => {
+                  setIsNavShowing((prev) => !prev);
+                  setTimeout(() => {
+                    LogOut();
+                  }, 2000);
+                }}
+                to={"/"}
+              >
+                log out
+              </NavLink>
+            </li>
+          )}
+          {/* unAuthenticated Routes*/}
+          {!auth && (
+            <>
+              <li>
+                <NavLink
+                  to="/login"
+                  onClick={() => setIsNavShowing((prev) => !prev)}
+                  className={"main-btn login-btn  nav-btn"}
+                >
+                  log in
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  onClick={() => setIsNavShowing((prev) => !prev)}
+                  className="main-btn login-btn nav-btn signUp-btn"
+                  to="/sign-up"
+                >
+                  Sign Up
+                </NavLink>
+              </li>
+            </>
+          )}
+        </ul>
+
+        <button
+          className="nav-toggle-btn"
+          onClick={() => setIsNavShowing((prev) => !prev)}
+        >
+          {isNavShowing ? <AiFillCloseSquare /> : <FaBars />}
+        </button>
       </div>
     </nav>
   );
